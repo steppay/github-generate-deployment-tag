92043f05a994967dd2139a2fcf2893c1183e8fed
Jihun <jihun@steppay.kr>
2023-03-21 17:23:37 +0900

    docs: inputs 명세

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import * as core from '@actions/core'
 
 async function run() {
     try {
-        const prefix = core.getInput('prefix')
         const { GITHUB_WORKSPACE, GITHUB_SHA, GITHUB_RUN_NUMBER } = process.env
         const repository = GITHUB_WORKSPACE.split('/').pop()
+        const prefix = core.getInput('prefix') || repository
         const slicedSha = GITHUB_SHA.slice(0, 7)
-        const tagName = `${prefix ? prefix : repository}-${GITHUB_RUN_NUMBER}-${slicedSha}`
+        const tagName = `${prefix}-${GITHUB_RUN_NUMBER}-${slicedSha}`
         core.setOutput('tag', tagName)
     } catch (error) {
         core.setFailed(error.message)
